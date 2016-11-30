8a51a19db0ba63aa67fdb1197e88b32780c83582
Minh-Long Do <minhlong.langos@gmail.com>
2016-11-30 20:18:20 +0000

    Callback event for form.

--- public/app.jsx
+++ public/app.jsx
@@ -4,13 +4,26 @@ var Greeter = React.createClass({
       name: 'React'
     };
   },
+  onButtonClick: function(e) {
+    //Prevent browser from refreshing the page
+    e.preventDefault();
+
+    var name = this.refs.name.value;
+
+    alert(name);
+  },
   render: function() {
     var name = this.props.name;
 
     return (
       <div>
-      <h1>Hello {name}!</h1>
-      <p>This is from a component</p>
+        <h1>Hello {name}!</h1>
+        <p>This is from a component</p>
+
+        <form onSubmit={this.onButtonClick}>
+          <input type="text" ref="name"/>
+          <button>Set Name</button>
+        </form>
       </div>
     );
   }
